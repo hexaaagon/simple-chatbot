c7e943ee9d992c10c177929b5c484686aa02ca1c
scoooolz7657 <scoooolz7657@gmail.com>
2024-12-10 10:33:57 +0000

    feat(ui): add theme toggle

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,5 +1,32 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export function Settings() {
-  return <></>;
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div className="absolute right-4 top-2">
+      <Button
+        size="icon"
+        onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
+      >
+        {resolvedTheme === "light" ? <Moon /> : <Sun />}
+        <p className="sr-only">Toggle Theme</p>
+      </Button>
+    </div>
+  );
 }
